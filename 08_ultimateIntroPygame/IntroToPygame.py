fc8a40c2ae3e36a0838120fc474bb14147472f17
uwmui <aidanjmoody32061@gmail.com>
2024-01-31 10:57:44 -0500

    update

--- 08_ultimateIntroPygame/IntroToPygame.py
+++ 08_ultimateIntroPygame/IntroToPygame.py
@@ -6,12 +6,17 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 
+sky_surface = pygame.image.load('08_ultimateIntroPygame\graphics\Sky.png')
+ground_surface = pygame.image.load('08_ultimateIntroPygame\graphics\ground.png')
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    # draw all our elements
-    # update everything
+
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,300))
+
     pygame.display.update()
     clock.tick(60)
