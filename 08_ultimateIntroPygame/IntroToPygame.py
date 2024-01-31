1deffc0324a02048d99bce11c0317c62f2b4632e
uwmui <aidanjmoody32061@gmail.com>
2024-01-31 12:11:56 -0500

    Update IntroToPygame.py

--- 08_ultimateIntroPygame/IntroToPygame.py
+++ 08_ultimateIntroPygame/IntroToPygame.py
@@ -11,8 +11,11 @@ sky_surface = pygame.image.load('08_ultimateIntroPygame\graphics\Sky.png').conve
 ground_surface = pygame.image.load('08_ultimateIntroPygame\graphics\ground.png').convert()
 text_surface = test_font.render('My game', False, 'Black')
 
-snail_surface = pygame.image.load('08_ultimateIntroPygame\graphics/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
+snail_surf = pygame.image.load('08_ultimateIntroPygame\graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(bottomright = (600,300))
+
+player_surf = pygame.image.load('08_ultimateIntroPygame\graphics/player/player_walk_1.png')
+player_rect = player_surf.get_rect(midbottom = (80,300))
 
 while True:
     for event in pygame.event.get():
@@ -23,9 +26,18 @@ while True:
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
     screen.blit(text_surface,(300,50))
-    snail_x_pos -= 4
-    if snail_x_pos < -100: snail_x_pos = 800
-    screen.blit(snail_surface,(snail_x_pos,250))
+
+    snail_rect.x -= 4
+    if snail_rect.right <= 0: snail_rect.left = 800
+    screen.blit(snail_surf,snail_rect)
+    screen.blit(player_surf,player_rect)
+
+    # if player_rect.colliderect(snail_rect):
+    #     print('collision')
+
+    mouse_pos = pygame.mouse.get_pos()
+    if player_rect.collidepoint((x,y)):
+        print(pygame.mouse.get_pressed())
 
     pygame.display.update()
     clock.tick(60)
