0c5c9fc5f29faf2d6db77af6625fb9d7407b8370
uwmui <aidanjmoody32061@gmail.com>
2024-04-12 13:20:55 -0400

    upd

--- 9_FinalProject/finalProject.py
+++ 9_FinalProject/finalProject.py
@@ -2,11 +2,17 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((800,400))
+screen = pygame.display.set_mode((1200,1000))
 pygame.display.set_caption('Test')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('Kenney Pixel Square.ttf', 50)
 game_active = True
 startTime = 0
+
+sky_surf = pygame.image.load()
+
+score_surf = test_font.render('My game', False, (64,64,64))
+score_rect = score_surf.get_rect(center = (400,50))
 
 while True:
     for event in pygame.event.get():
