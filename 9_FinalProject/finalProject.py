0161a76b05b5c0d6a5a4b41e74ba545e4c01bf4f
Ryan K. <kelleyr@duvalschools.org>
2024-04-18 13:19:48 -0400

    Code Review Test -- April 18, 2024

--- 9_FinalProject/finalProject.py
+++ 9_FinalProject/finalProject.py
@@ -9,7 +9,7 @@ game_active = True
 startTime = 0
 
 title_surf = pygame.image.load('9_FinalProject\graphics/titlescreenbeta.png').convert()
-title_rect = title_surf.get_rect(center = (0,0))
+title_rect = title_surf.get_rect()
 
 while True:
     for event in pygame.event.get():
@@ -17,11 +17,21 @@ while True:
             pygame.quit()
             exit()
     
-    if game_active:
-        screen.blit(title_surf,(0,0))
-        pygame.draw.rect(screen,'#c0e8ec',title_rect)
-        pygame.draw.rect(screen,'#c0e8ec',title_rect,10)
-        screen.blit(title_surf, title_rect)
+    if game_active: # What happens each 'turn' or frame of the game. 
+        screen.blit(title_surf, (0,0))
+        #pygame.draw.rect(screen,'#c0e8ec',title_rect)
+        #pygame.draw.rect(screen,'#c0e8ec',title_rect,10)
+        #screen.blit(title_surf, title_rect)
+
+        # transition from title screen to level 1. 
+        # Display Level 1. 
+        # Display Player Sprite. 
+        # Display HUD 
+        # Display Enemies 
+        # Display Items 
+
+
+
 
     pygame.display.update()
     clock.tick(60)
